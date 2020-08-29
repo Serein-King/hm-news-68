59dd9c068175093c56be160092c1b92af1bb80f2
serein-king <18721369858@163.com>
2020-08-29 16:14:33 +0800

    配置了vant

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/base.less'
+import './styles/iconfont.css'
+// 全局的把 vant组件都导入好了
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
+// 自动按需引入组件 (推荐)
+// import { Button } from 'vant'
+// Vue.use(Button)
+// import Button from 'vant/lib/button';
+// import 'vant/lib/button/style';
 Vue.config.productionTip = false
 new Vue({
   router,
